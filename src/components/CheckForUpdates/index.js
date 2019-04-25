88836d317faece300bdc9cf77a3faf59074e7d75
Carter Harrison <carterjharrison@gmail.com>
2019-04-25 12:20:50 -0700

    remove duplicate pending dots view

--- src/components/CheckForUpdates/index.js
+++ src/components/CheckForUpdates/index.js
@@ -4,11 +4,11 @@ import Loader from '@xyo-network/tool-storybook-react/dist/lib/Loader'
 
 export default ({ className, onSuccess }) => {
   return (
-    <CheckForUpdates update={onSuccess}>
+    <CheckForUpdates update={onSuccess} >
       {(checkForUpdates, { loading: updating }) => (
         updating
-        ? <Loader className={className} />
-        : <a className={className} onClick={checkForUpdates} >Check for updates</a>
+        ? <a className={className} >Pending...</a>
+        : <a className={className} onClick={checkForUpdates} >Refresh</a>
     )}
     </CheckForUpdates>
   )
